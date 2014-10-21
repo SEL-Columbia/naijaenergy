627c596f5408de6e48a927111e64d37ebf000c75
Myf Ma <myf@worldtalks.org>
2014-10-21 11:57:40 -0400

    updating db setup script

--- server/reducer.js
+++ server/reducer.js
@@ -4,7 +4,10 @@ var _ = require('underscore');
 var mapreduce = require('map-reduce');
 //var data_db = sub(level('db/data', {valueEncoding: 'json'}));
 var data_db =require('./levelmedown').data_db;
+var geojson_db =require('./levelmedown').geojson_db;
 var mapper = require('./levelmedown').mapdb;
 var write_data_db = require('./levelmedown').write_data_db;
+var write_geojson_db = require('./levelmedown').write_geojson_db;
 
+write_geojson_db('lgas.json', 'states.json', geojson_db);
 write_data_db('raw.json', data_db);
